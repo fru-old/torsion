9968f48dc12f0a627c65a65fa7af067e880ad968
x-200 <flo@ubuntu.ubuntu-domain>
2013-07-23 16:32:52 +0200

    Delete

--- src/com/github/fru/torsion/javascript/JsNative.java
+++
@@ -1,9 +0,0 @@
-package com.github.fru.torsion.javascript;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface JsNative {
-	String[] inline() default "";
-}
